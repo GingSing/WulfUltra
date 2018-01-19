0ab8358df62259f0f625d182d2c7353ec66dc3da
Gary Yue <garyyue2017@gmail.com>
2018-01-18 17:48:51 -0800

    1/18/2018
    Added a scrollbar

--- static/javascript/functionality.js
+++ static/javascript/functionality.js
@@ -1,1 +1,42 @@
-alert("hello");
+var myTimeOut;
+
+$(".progressBar").hide();
+$(".progressContainer").hide();
+
+$(window).ready(function(){
+$(window).scroll(function(){
+  var wintop = $(window).scrollTop(),
+  docheight = $(document).height(),
+  winheight = $(window).height();
+
+  var totalScroll = (wintop/(docheight - winheight)) * 100;
+  $(".progressBar").css("height", totalScroll + "%")
+
+  if(totalScroll != 0){
+  $(".progressBar").show();
+  $(".progressContainer").show();
+  clearTimeout(myTimeOut);
+  myTimeOut = setTimeout(function(){
+  $(".progressBar").css("height", "0%");
+  $(window).scrollTop(0);
+  $(".progressBar").hide();
+  $(".progressContainer").hide();
+  }, 500);
+  }
+});
+
+});
+
+function detectMouseWheelDirection(e){
+
+    var delta = null,
+        direction = false;
+
+    if (!e){
+        e = window.event;
+    }
+    if(e.wheelDelta){
+
+    }
+
+}
